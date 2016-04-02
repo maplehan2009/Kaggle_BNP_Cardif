1f991b9722fce7ff92ff14777c259e4bf37adbde
maplehan2009 <maplehan2009@gmail.com>
2016-04-02 23:15:57 +0200

    first trial

--- main.py
+++ main.py
@@ -6,4 +6,39 @@ Created on Mon Mar 28 23:45:08 2016
 """
 
 import pandas as pd
-m = pd.read_csv('data/train.csv')
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
+from sklearn import svm
+from sklearn import cross_validation
+from sklearn.linear_model import LogisticRegression
+
+def data_pre_processing(m):
+    #m = m.fillna(0)
+    m_numeric = m[m.describe().columns.values]
+    m_numeric = m_numeric.dropna() 
+    return m_numeric
+
+def feature_selection(m):
+    y = m.values[:, 1]
+    X = m.values[:, 2:]
+    X_new = SelectKBest(f_classif, k=25).fit_transform(X, y)
+    return X_new, y
+
+def learning_algo_svm(X, y):
+    clf = svm.SVC().fit(X_train, y_train)
+    return clf
+    
+def learning_algo_logit(X, y):
+    clf = LogisticRegression().fit(X, y)
+    return clf
+    
+
+
+if __name__=='__main__':
+    m = pd.read_csv('data/train.csv')
+    m = data_pre_processing(m)
+    X, y = feature_selection(m)
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split( X, y, test_size = 0.3, random_state=0)
+    clf = learning_algo_logit(X_train, y_train)
+    score = clf.score(X_test, y_test)
+    print score
