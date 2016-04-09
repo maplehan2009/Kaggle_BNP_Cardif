c8b1722d716658788d9b1ee861a979f52d041cae
maplehan2009 <maplehan2009@gmail.com>
2016-04-09 19:53:50 +0200

    first version of submission

--- main.py
+++ main.py
@@ -12,41 +12,54 @@ from sklearn import svm
 from sklearn import cross_validation
 from sklearn.linear_model import LogisticRegression
 from sklearn.decomposition import PCA 
+from sklearn.feature_selection import SelectFromModel
 
 def data_pre_processing(m):
     m = m.fillna(0)
-    m_numeric = m[m.describe().columns.values]
-    #m_numeric = m_numeric.dropna() 
+    m_numeric = m[m.describe().columns.values] 
     return m_numeric
 
-def feature_selection(m):
-    y = m.values[:, 1]
-    X = m.values[:, 2:]
-    X_new = SelectKBest(f_classif, k=25).fit_transform(X, y)
-    return X_new, y
+def feature_selection(m, t):
+    y_train = m.values[:, 1]
+    X_train = m.values[:, 2:]
+    X_test = t.values[:, 1:]
+    
+    idx = SelectKBest(f_classif, k=5).fit(X_train, y_train).get_support()
+    X_train_new = X_train[:, idx]
+    X_test_new = X_test[:, idx]
+    return X_train_new, y_train, X_test_new
 
-def decomposition_PCA(m):
-    y = m.values[:, 1]
-    X = m.values[:, 2:]	
-    pca = PCA(n_components=2)
-    X_new = pca.fit_transform(X) 
-    return X_new, y
+def decomposition_PCA(m, t):
+    y_train = m.values[:, 1]
+    X_train = m.values[:, 2:]	
+    X_test = t.values[:, 1:]
+    
+    pca = PCA(n_components=10)
+    X_train_new = pca.fit_transform(X_train)
+    X_test_new = pca.fit_transform(X_test)
+    
+    return X_train_new, y_train, X_test_new
 
 def learning_algo_svm(X, y):
-    clf = svm.SVC().fit(X_train, y_train)
+    clf = svm.SVC(probability=True).fit(X_train, y_train)
     return clf
     
 def learning_algo_logit(X, y):
     clf = LogisticRegression().fit(X, y)
     return clf
-    
 
 
 if __name__=='__main__':
     m = pd.read_csv('data/train.csv')
+    t = pd.read_csv('data/test.csv')
     m = data_pre_processing(m)
-    X, y = decomposition_PCA(m)
-    X_train, X_test, y_train, y_test = cross_validation.train_test_split( X, y, test_size=0.3, random_state=0)
+    t = data_pre_processing(t)
+    
+    #X_train, y_train, X_test = decomposition_PCA(m, t)
+    X_train, y_train, X_test = feature_selection(m, t)
     clf = learning_algo_logit(X_train, y_train)
-    score = clf.score(X_test, y_test)
-    print score
+    #clf = learning_algo_svm(X_train, y_train)
+    y_test = clf.predict_proba(X_test)[:, 1]
+    submission = pd.DataFrame(y_test, index = t.ID, columns=['PredictedProb'])
+    submission.to_csv('data/my_submission.csv')
+    
